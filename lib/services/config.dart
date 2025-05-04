de08d7a1ab471de825a2aa924e853556645aa566
Mahdi <mghorbel1@yahoo.fr>
2025-05-04 22:39:26 +0100

    commented using token message

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -2,7 +2,7 @@
 
 class Config {
   // Base URL for your backend API – update this to your actual backend URL.
-  static const String baseUrl = 'http://10.0.2.2:8000';
+  static const String baseUrl = 'http://127.0.0.1:5001';
 
   // Authentication
   static const String authTokenUrl = '$baseUrl/auth/token';
